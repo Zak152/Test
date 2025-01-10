b7592d84027ecd0d88c0d2bfc48884fbe34e5f68
Kai <durak7609@gmail.com>
2025-01-10 15:47:17 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print('Hello World')
 print('Have a good day')
 print(5+10)
+a = input('Как дела? ')
+if a == 'Нормально':
+    print('У меня тоже')
+elif a == 'Отлично':
+    print('Хорошо!')
+else:
+    print('Понял!')
